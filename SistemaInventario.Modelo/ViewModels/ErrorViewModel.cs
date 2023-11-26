f631ed4b37efee0698fa304df13aeef2f527bafb
Iamccdev28 <149208643+Iamccdev28@users.noreply.github.com>
2023-11-26 02:05:27 -0600

    configuración del proyecto

---
+++ SistemaInventario.Modelo/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventario.Modelo.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
