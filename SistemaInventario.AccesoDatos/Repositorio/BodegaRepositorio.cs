bde03f619bd3eb84b6abc6ac2c05fb337729b087
Iamccdev28 <149208643+Iamccdev28@users.noreply.github.com>
2023-12-17 18:40:26 -0600

    Patron de repositorio

---
+++ SistemaInventario.AccesoDatos/Repositorio/BodegaRepositorio.cs
@@ -0,0 +1,30 @@
+﻿using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class BodegaRepositorio : Repositorio<Bodega>, IBodegaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+        public BodegaRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Actualizar(Bodega bodega)
+        {
+            var bodegaBd = _db.Bodegas.FirstOrDefault(b => b.Id == bodega.Id);
+            if (bodegaBd != null)
+            {
+
+                bodega.Nombre = bodegaBd.Nombre;
+                bodega.Descripcion = bodegaBd.Descripcion;  
+                bodega.Estado = bodegaBd.Estado;            
+                _db.SaveChanges();
+        }
+    }
+}
